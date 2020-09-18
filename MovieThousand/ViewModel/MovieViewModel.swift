625f3061e823814f52c17526eaa2546c2d3afb0b
SwiftTanirbergen <developer.tanirbergen@mail.ru>
2020-09-18 17:18:27 +0600

    каркас viewmdoel

---
+++ MovieThousand/ViewModel/MovieViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  MovieViewModel.swift
+//  MovieThousand
+//
+//  Created by Devnull on 9/18/20.
+//  Copyright © 2020 Devnull. All rights reserved.
+//
+
+import Foundation
+
+class MovieViewModel {
+    
+    var movies = [Movie]()
+    
+    init() {
+        
+    }
+    // api
+    func getMovies(endpoint : String?) {
+        
+    }
+    //rowItem
+    func getMovie(at indexPatch : String?){
+        
+    }
+    //search
+    func getSearch(with query : String?){
+        
+    }
+}
