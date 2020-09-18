a7a95a1cb4f900252de73de034417185ebf7e973
SwiftTanirbergen <developer.tanirbergen@mail.ru>
2020-09-18 17:04:44 +0600

    adding collectionView to mainController

---
+++ MovieThousand/ViewModel/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  MovieThousand
+//
+//  Created by Devnull on 9/18/20.
+//  Copyright © 2020 Devnull. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+//        let search = UISearchController(se)
+    }
+
+    
+    // Collection View
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = UIScreen.main.bounds.width
+        return CGSize(width: width, height: 400)
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellid", for: IndexPath)
+        return cell
+    }
+    
+ 
+}
+
